2ca57c866fca53cdac1a3c9b0c115103dddb4ea3
Tom Williams <tomwilliams@Toms-MacBook-Pro.local>
2023-01-14 11:15:58 -0800

    running app

---
+++ app/src/App.tsx
@@ -0,0 +1,9 @@
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline m-4 p-4 border border-black">
+      Hello world!
+    </h1>  )
+}
+
+export default App
---
+++ app/src/exampleTest.spec.tsx
@@ -0,0 +1,6 @@
+import { describe, it, expect } from 'vitest';
+describe('example', () => {
+    it('runs a test', () => {
+        expect(true).toBeTruthy();
+    })
+})
